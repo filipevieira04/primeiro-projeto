c8905f2a2b243fb9dbea058dbe9168842bdbabfc
Filipe Vieira <flypevieira@gmail.com>
2026-09-10 02:10:56 -0300

    Remove game rules comments from jogo de batalha.py
    Removed initial comments outlining game rules and character details.

--- jogo de batalha.py
+++ jogo de batalha.py
@@ -1,20 +1,3 @@
-"""
-Cada Personagem vai ter: energia , ataque , vida , especial , nome , defesa ;
-
-Vão ser 5 personagens: Cavaleiro , Mago , Arqueiro , Ogro , Dragão;
-                           2         3        1        4       6
-Vão ser 2 Jogadores: Cada um começará com 10 fichas, que serão usadas para comprar os personagens acima ;
-
-Ganha quem derrotar todos os personagens do outro jogador ;
-
-O especial de cada personagem será recarregado (+5 de energia) conforme o passar dos turnos ;
-
-Em cada turno o jogador deve decidir qual personagem ele quer atacar e qual personagem ele usará
-(mas, dependendo do especial, o ataque pode ser múltiplo) ;
-
-Cada personagem terá uma defesa padrão automática;
-"""
-
 import os
 import subprocess
 from tabulate import tabulate
